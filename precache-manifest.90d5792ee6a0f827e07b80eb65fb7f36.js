08e93c7b9719cb4e0c302096f7f232ea551ba20d
soldier <sodier@dcpt.online>
2019-02-10 20:39:29 +0800

    1

---
+++ precache-manifest.90d5792ee6a0f827e07b80eb65fb7f36.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "d74924be78e93ed88ce698212dbada55",
+    "url": "/static/media/og-bg.d74924be.png"
+  },
+  {
+    "revision": "d466a48416278d24cd55",
+    "url": "/static/css/main.e2dbd5c1.chunk.css"
+  },
+  {
+    "revision": "0ad5cd449fb005a6d0d186e90b720b5d",
+    "url": "/static/media/bg1.0ad5cd44.png"
+  },
+  {
+    "revision": "543a9392c50d556458f2",
+    "url": "/static/js/1.543a9392.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6d2e36c02950921645e2f51adf10f347",
+    "url": "/static/media/lottery.6d2e36c0.png"
+  },
+  {
+    "revision": "f2a4d466e22efffb336da14a14398f81",
+    "url": "/static/media/company-bg.f2a4d466.png"
+  },
+  {
+    "revision": "662f12c7be26d96cb5a1793a00482714",
+    "url": "/static/media/agency-bg.662f12c7.png"
+  },
+  {
+    "revision": "47af58cc7df79f317685f4068366f971",
+    "url": "/static/media/lottery-bg.47af58cc.png"
+  },
+  {
+    "revision": "6f214f8c8e7f13f6f78bc02f2a45dfc0",
+    "url": "/static/media/egame-bg.6f214f8c.png"
+  },
+  {
+    "revision": "d79cf100ff3b49a1a7dcd0cafc97f55b",
+    "url": "/static/media/chess-bg.d79cf100.png"
+  },
+  {
+    "revision": "d466a48416278d24cd55",
+    "url": "/static/js/main.d466a484.chunk.js"
+  },
+  {
+    "revision": "53ad73f5e44214e98c196a32e0980f4d",
+    "url": "/static/media/phone.53ad73f5.png"
+  },
+  {
+    "revision": "8be7f4f80682e4270f26e9b6089aa4fe",
+    "url": "/static/media/telegram.8be7f4f8.png"
+  },
+  {
+    "revision": "604132fd2888da6e27a1cde73efd42e7",
+    "url": "/static/media/whatsapp.604132fd.png"
+  },
+  {
+    "revision": "da673e17da2d9fbe3109d66d8902b5c3",
+    "url": "/static/media/bg2.da673e17.png"
+  },
+  {
+    "revision": "33c04b0bd4d82d695615f583d1526431",
+    "url": "/static/media/bg6.33c04b0b.png"
+  },
+  {
+    "revision": "01fd0bafa862b47ffccc381e612d7304",
+    "url": "/static/media/company-logo.01fd0baf.png"
+  },
+  {
+    "revision": "5f9ae5ea79a502eeb4d0a4ed3db7db10",
+    "url": "/static/media/bg5.5f9ae5ea.png"
+  },
+  {
+    "revision": "37dc71699ae8e3053bdfae5abb162468",
+    "url": "/static/media/bg3.37dc7169.png"
+  },
+  {
+    "revision": "a85c4b97a0cce95660dadea72b550217",
+    "url": "/static/media/bg4.a85c4b97.png"
+  },
+  {
+    "revision": "543a9392c50d556458f2",
+    "url": "/static/css/1.9d445f59.chunk.css"
+  },
+  {
+    "revision": "211eeac08ae460de5cca5e602e975d71",
+    "url": "/index.html"
+  }
+];
