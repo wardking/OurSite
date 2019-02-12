1415ffd45d55e31cdd7582b9deac7319beac5a15
xiaona <xiaona@dcpt.online>
2019-02-12 19:52:31 +0800

    1

--- src/index.js
+++ src/index.js
@@ -118,14 +118,14 @@ class App extends React.Component {
                            </div>
                        </div>
                    </div>
-
+                    {/*创新彩种*/}
                    <div style={this.state.size}>
                        <div className="container bg3" >
                            <Logo white={1} />
                            <DownPage goNext={this.goNext} />
                            <div className="content new-lottery-wrap">
                                <div className="head-title"/>
-                               <p className="description">恒博彩票創新自主彩種，搭配市場熱門彩種，擁有几十種彩票種類。即時開獎數據、彩票資訊、統計及分析服務。不同於其他彩票平台，特別開發智慧操作、投注簡易的界面。</p>
+                               <p className="description">恒博彩票创新自主彩种，搭配市场热门彩种，拥有几十种彩票种类。即时开奖数据、彩票资讯、统计及分析服务。不同于其他彩票平台，特别开发智慧操作、投注简易的界面。</p>
                                <img  className="new-lottery-img" src={lotteryImg} alt=""/>
                            </div>
                        </div>
@@ -136,8 +136,11 @@ class App extends React.Component {
                            <DownPage goNext={this.goNext} />
                            <div className="content agency-wrap">
                                <div className="head-title "/>
-                               <p className="description">您的佣金构成不仅来自于会员，更有代理进账返佣。根据业绩的高低，划分不同佣金比例和会员等级，您的自营越高，代理越多相对应
-                                   的等级越高，返佣越多。<span>无限代理</span><span>无限封顶</span><span>共同助力</span><span>日日进账</span></p>
+                               <div className='lotteryCont'>
+                                   <span className="description">您的佣金构成不仅来自于会员，更有代理进账返佣。根据业绩的高低，划分不同佣金比例和会员等级，您的自营越高，代理越多相对应
+                                       的等级越高，返佣越多。</span>
+                                   <span className='desList'>无限代理</span><span className='desList'>无限封顶</span><span className='desList'>共同助力</span><span className='desList'>日日进账</span>
+                               </div>
                                <img className="agency-img" src={agencyBg} alt="全名代理"/>
                            </div>
 
@@ -150,7 +153,7 @@ class App extends React.Component {
                             <div className="content lottery-wrap">
                                 <div className="head-title"/>
 
-                                <p className="description">恒博 DC 彩票創新自主彩種，搭配市場熱門彩種，擁有几十種彩票種類。即時開獎數據、彩票資訊、統計及分析服務。不同於其他彩票平台，特別開發智慧操作、投注簡易的界面。</p>
+                                <p className="description">恒博DC 彩票创新自主彩种，搭配市场热门彩种，拥有几十种彩票种类。即时开奖数据、彩票资讯、统计及分析服务。不同于其他彩票平台，特别开发智慧操作、投注简易的界面。</p>
                                 <img className="lottery-img" src={lotteryBg} alt="恒博新彩种"/>
                             </div>
                        </div>
@@ -162,7 +165,7 @@ class App extends React.Component {
                            <DownPage type={2} goNext={this.goNext} />
                            <div className="content egame-wrap">
                                <div className="head-title "/>
-                               <p className="description">恒博游戏，市場最新，恒博娛樂與供應商間絕佳合作關係，特接入多超過数十款電子遊戲產品，包含人人喜歡刺激的千炮捕鱼、大满贯MW电子、斗地主以及森林舞会等項目；恒博娛樂憑借整合技術優勢，隨時保持鮮活趣味，提供玩家最佳互動體驗</p>
+                               <p className="description">恒博游戏，市场最新，恒博娱乐与供应商间绝佳合作关系，特接入多超过数十款电子游戏产品，包含人人喜欢刺激的千炮捕鱼、大满贯MW电子、斗地主以及森林舞会等项目；恒博娱乐凭借整合技术优势，随时保持鮮活趣味，提供玩家最佳互动体验。</p>
                                <img className="egame-bg" src={egameBg} alt="棋牌"/>
                            </div>
                        </div>
@@ -174,7 +177,7 @@ class App extends React.Component {
                            <DownPage goNext={this.goNext} />
                             <div className="content og-game-wrap">
                                 <div className="head-title"/>
-                                <p className="og-game-description">恒博娱乐提供多款真人視訊遊戲平台，提供高清視頻流及無限遊戲玩法。超越業眾步調，接入知名品牌產品，集成东方集团旗下的OG视讯等款遊戲。憑藉我們的純熟快速的技術團隊，提供穩定性高、畫面清晰的播界面，讓玩家更即時精准，如同親臨現場娛樂場所。我們的優勢經驗，為各運營商提供最佳的解決方案，不論是單獨介接遊戲或是集成網站，都將超乎您所想像。
+                                <p className="og-game-description">恒博娱乐提供多款真人视讯游戏平台，提供高清视频流及无限游戏玩法。超越业众步调，接入知名品牌产品，集成东方集团旗下的OG视讯等款游戏。凭借我们的純熟快速的技术团队，提供稳定性高、画面清晰的播界面，让玩家更即时精准，如同亲临现场娱乐场所。我们的优势经验，为各运营商提供最佳的解決方案，不论是单独介接游戏或是集成网站，都将超乎您所想像。
                                     <img className="og-game-bg2" src={ogBg2} alt=""/>
                                 </p>
                                 <img className="og-game-bg"  src={ogBg} alt=""/>
@@ -187,21 +190,22 @@ class App extends React.Component {
                            <DownPage goNext={this.goNext} />
                            <div className="content chess-wrap">
                                <div className="head-title"/>
-                               <p className="description">恒博娛樂引进目前国际流行的棋牌游戏，包括开元棋牌。乐游 VG棋牌也将陆续开启，引领市场，给您不一样的绝佳体验。</p>
+                               <p className="description">恒博娱乐引进目前国际流行的棋牌游戏，包括开元棋牌。乐游 VG棋牌也将陆续开启，引领市场，给您不一样的绝佳体验。</p>
                                <img className="chess-bg" src={chessBg} alt="棋牌"/>
                            </div>
                        </div>
                    </div>
+                   {/*集团介绍*/}
                    <div style={this.state.size}>
                        <div className="container bg2 ">
                            <Logo />
                            <DownPage type={2} goNext={this.goNext} />
                            <div className="content company-wrap">
                                <div className="head-title"/>
-                               <img width="535" src={companyBg} alt=""/>
                                <div className="company-info">
-                                   恒博娱乐以丰富的研發力、專業的服務力、全面的策略力，為您開創新局、提升規模、整合策略、建立品牌，與我們合作將在商場上持續獲利、高枕無憂。作為一家亞洲實力的軟件公司，解決方案，平台規劃、活動規劃、市場營銷一應俱全，為現有系統商中最全局宏觀的策略顧問公司。恒博娱乐具備資深經驗及相關知識。與客戶之間持續成功、雙贏的合作關係，並確保提供最完美的在線娛樂。
+                                   恒博娱乐以丰富的研发力、专业的服务力、全面的策略力，为您开创新局、提升规模、整合策略、建立品牌，与我们合作将在商场上持续获利、高枕无忧。作为一家亚洲实力的软件公司，解决方案，平台规划、活動规划、市场营销一应俱全，为现有系統商中最全局宏观的策略顾问公司。恒博娱乐具备资深经验及相关知识。与客户之间持续成功、双赢的合作关系，並确保提供最完美的在线娱乐。
                                </div>
+                               <img width="535" src={companyBg} alt=""/>
                            </div>
                        </div>
                    </div>
